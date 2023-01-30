5a26e85a3ad9bb764036d8dbe3bcd6f8d06407dd
L0nelyR <95019928+L0nelyR@users.noreply.github.com>
2023-01-30 17:17:23 -0500

    Added entity system and camera movement

--- src/main/java/com/deadlist/core/RenderManager.java
+++ src/main/java/com/deadlist/core/RenderManager.java
@@ -1,6 +1,8 @@
 package com.deadlist.core;
 
+import com.deadlist.core.entity.Entity;
 import com.deadlist.core.entity.Model;
+import com.deadlist.core.utils.Transformation;
 import com.deadlist.core.utils.Utils;
 import com.deadlist.test.Launcher;
 import org.lwjgl.opengl.GL11;
@@ -23,18 +25,25 @@ public class RenderManager {
         shader.createFragmentShader(Utils.loadResource("/shaders/fragment.glsl"));
         shader.link();
         shader.createUniform("textureSampler");
+        shader.createUniform("transformationMatrix");
+        shader.createUniform("projectionMatrix");
+        shader.createUniform("viewMatrix");
     }
 
-    public void render(Model model){
+    public void render(Entity entity, Camera camera){
         clear();
         shader.bind();
         shader.setUniform("textureSampler", 0);
-        GL30.glBindVertexArray(model.getId());
+        shader.setUniforms("transformationMatrix", Transformation.createTransformationMatrix(entity));
+        shader.setUniforms("projectionMatrix", window.updateProjectionMatrix());
+        shader.setUniforms("viewMatrix", Transformation.getViewMatrix(camera));
+
+        GL30.glBindVertexArray(entity.getModel().getId());
         GL20.glEnableVertexAttribArray(0);
         GL20.glEnableVertexAttribArray(1);
         GL13.glActiveTexture(GL13.GL_TEXTURE0);
-        GL11.glBindTexture(GL11.GL_TEXTURE_2D, model.getTexture().getId());
-        GL11.glDrawElements(GL11.GL_TRIANGLES, model.getVertexCount(), GL11.GL_UNSIGNED_INT, 0);
+        GL11.glBindTexture(GL11.GL_TEXTURE_2D, entity.getModel().getTexture().getId());
+        GL11.glDrawElements(GL11.GL_TRIANGLES, entity.getModel().getVertexCount(), GL11.GL_UNSIGNED_INT, 0);
         GL20.glDisableVertexAttribArray(0);
         GL20.glDisableVertexAttribArray(1);
         GL30.glBindVertexArray(0);
