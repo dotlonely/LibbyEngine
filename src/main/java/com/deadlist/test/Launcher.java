4b2a9f1b767013625f51fa6002da042aab95a620
L0nelyR <95019928+L0nelyR@users.noreply.github.com>
2023-01-29 19:14:30 -0500

    Can render models to screen now

--- src/main/java/com/deadlist/test/Launcher.java
+++ src/main/java/com/deadlist/test/Launcher.java
@@ -7,11 +7,13 @@ import org.lwjgl.Version;
 public class Launcher {
 
     private static WindowManager window;
-    private static EngineManager engine;
+    private static TestGame game;
+
 
     public static void main(String[] args) {
         window = new WindowManager(Consts.TITLE, 1024, 768, false);
-        engine = new EngineManager();
+        game = new TestGame();
+        EngineManager engine = new EngineManager();
 
        try {
             engine.start();
@@ -22,5 +24,9 @@ public class Launcher {
 
     public static WindowManager getWindow() {
         return window;
+    }
+
+    public static TestGame getGame(){
+        return game;
     }
 }
